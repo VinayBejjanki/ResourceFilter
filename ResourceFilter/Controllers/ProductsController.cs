c1590a7716af2efa0e77d84c7662a6c84eb7e03b
VinayBejjanki <bejjankivinay002@gmail.com>
2025-09-10 00:19:14 +0530

    Initial commit – added Controllers, Program.cs, Resources

---
+++ ResourceFilter/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResourceFilter.ResourceFilter;
+
+namespace ResourceFilter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        [CacheFilter]
+        public async Task<IActionResult> Getproduct()
+        {
+            var products = new List<string>
+            {
+                "Laptop",
+                "Modile",
+                "Cell phones",
+                "Clothes"
+            };
+
+            return Ok(products);
+        }
+    }
+}
---
+++ ResourceFilter/ResourceFilter/CacheFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
+
+namespace ResourceFilter.ResourceFilter
+{
+    public class CacheFilter : Attribute, IResourceFilter
+    {
+        public static Dictionary<string, object> _cache = new Dictionary<string, object>();
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            var path = context.HttpContext.Request.Path.ToString();
+
+            if (_cache.ContainsKey(path))
+            {
+                context.Result = new OkObjectResult(_cache[path]);
+                Console.WriteLine($"Cache HIT for {path}");
+            }
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            var path = context.HttpContext.Request.Path.ToString();
+
+            if (context.Result is ObjectResult result)
+            {
+                _cache[path] = result.Value!;
+                Console.WriteLine($"Cache STORED for {path}");
+            }
+        }
+    }
+}
+
